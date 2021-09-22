5e12d54cce5ebd53b3e9c519c793afcd5a1f3b47
stifler33 <bagi33.ru@gmail.com>
2021-09-22 20:12:37 +0300

    full file puch

---
+++ cmake-build-debug/CMakeFiles/21_5.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "21_5.exe"
+  "21_5.exe.manifest"
+  "21_5.pdb"
+  "CMakeFiles/21_5.dir/main.cpp.obj"
+  "lib21_5.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/21_5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -1,50 +1,85 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <vector>
 using namespace std;
-struct strBill{
-    string name = "";
-    int salary = 0;
-    string date = "";
+struct bills{
+    string first_name;
+    string last_name;
+    string date;
+    int salary;
 };
-void addNote(fstream& file, strBill& note){
-    int length_name = note.name.length();
-    file.write(note.name.c_str(), length_name);
-    int length_date = note.date.length();
-    file.write(note.date.c_str(), length_date);
-    file.write((char*) &note.salary, sizeof(note.salary));
-}
-void addVecBill(vector<strBill> &vecBill){
-    strBill noteBill;
-    vecBill.push_back(noteBill);
-    int indexVec = vecBill.size() - 1;
-    cout << "Enter name :\n";
-    getline(cin,vecBill[indexVec].name, '\n');
-    cout << "Enter salary :\n";
-    cin >> vecBill[indexVec].salary;
-    cout << "Enter date :\n";
-    cin >> vecBill[indexVec].date;
-}
-int main() {
-    //strBill to_strBill;
-    fstream f_bills("bills.bin", ios::binary | ios::in | ios::out);
-    if (!f_bills.is_open()){
-       ofstream out("bills.bin", ios::binary);
-       cout << "File added !\n";
-   }
-    vector<strBill> vecBill;
-    string option;
-    while (option != "q"){
-        cout << "Enter option\n";
-        getline(cin , option);
-        if (option == "add"){
-            addVecBill(vecBill);
-        }
-        for (int i = 0; i < vecBill.size(); i++){
-            cout << vecBill[i].name << " " << vecBill[i].salary << " " << vecBill[i].date << endl;
-            cout << "**************************\n";
+bills note;
+bool add_in_data(fstream &data ,string flag, char sy = ' ',string textUser = ""){
+
+    if (flag == "fullName"){
+        cout << "First name\n";
+        cin >> note.first_name;
+        cout << "Last name\n";
+        cin >> note.last_name;
+        textUser = note.first_name + " " + note.last_name;
+        for (int i = 0; i < textUser.size(); i++){
+            if (textUser[i] >= '0' && textUser[i] <= '9'){
+                std::cerr << "error format full name !\n";
+                return false;
+            }
         }
     }
+    if (flag == "date"){
+        cin >> textUser;
+        if (textUser.size() != 10) {
+            cerr << "error is long date\n";
+            return false;
+        }
+        if (stoi(textUser.substr(0,2)) < 0 || stoi(textUser.substr(0,2)) > 31){
+            cerr << "error format date - day\n";
+            return false;
+        }
+        if (stoi(textUser.substr(3,2)) < 0 || stoi(textUser.substr(3,2)) > 12){
+            cerr << "error format date - month\n";
+            return false;
+        }
+        if (stoi(textUser.substr(6,4)) < 1999 || stoi(textUser.substr(6,4)) > 2024){
+            cerr << "error format date - year\n";
+            return false;
+        }
+    }
+    if (flag == "salary"){
+        cin >> textUser;
+        for (char i : textUser){
+            if (i < '0' || i > '9'){
+                cerr << "error format salary\n";
+                return false;
+            }
+        }
+    }
+    data << textUser << sy;
+    return true;
+}
+int main() {
+    fstream data("register.txt", ios::in | ios::out);
+    if (!data.is_open()){
+        fstream init("register.txt", ios::out | ios::trunc);
+        init.close();
+    }
+    string stopWord;
+    string option;
+    cout << "enter option\n";
+    cin >> option;
+    if (option == "add") {
+        while (stopWord != "n") {
+            do {
+                cout << "Enter full name :\n";
+            } while (!add_in_data(data, "fullName"));
+            do {
+                cout << "Enter date :\n";
+            } while (!add_in_data(data, "date"));
+            do {
+                cout << "Enter sum salary :\n";
+            } while (!add_in_data(data, "salary", '\n'));
+            cout << "proceed ?\n";
+            cin >> stopWord;
+        }
+    }
+    data.close();
     return 0;
 }
