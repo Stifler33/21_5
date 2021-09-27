0e9bcd530dc89d677abb5f251c5e131c12cbcdf5
stifler33 <bagi33.ru@gmail.com>
2021-09-27 10:59:36 +0300

    task4 added person , input field moving person

--- task4.cpp
+++ task4.cpp
@@ -13,36 +13,43 @@ struct one{
     int hp;
     int damage;
     int armor;
+    int crd[2];
 };
 struct two{
     string name = "Enemy_#2";
     int hp;
     int damage;
     int armor;
+    int crd[2];
 };
 struct three{
     string name = "Enemy_#3";
     int hp;
     int damage;
     int armor;
+    int crd[2];
 };
 struct four{
     string name = "Enemy_#4";
     int hp;
     int damage;
     int armor;
+    int crd[2];
 };
 struct five{
     string name = "Enemy_#5";
     int hp;
     int damage;
     int armor;
+    int crd[2];
 };
 struct user{
     string name = "User";
     int hp;
     int damage;
     int armor;
+    //coordinates crd[x,y]
+    int crd[2];
 };
 struct person{
     one One;
@@ -72,13 +79,115 @@ void initEnemy(person &Person){
     Person.Three.damage = getRandNumb(15,30);
     Person.Four.damage = getRandNumb(15,30);
     Person.Five.damage = getRandNumb(15,30);
+    // init coordinates crd[x,y]
+    Person.One.crd[0] = getRandNumb(0,40);
+    Person.One.crd[1] = getRandNumb(0,40);
+    Person.Two.crd[0] = getRandNumb(0,40);
+    Person.Two.crd[1] = getRandNumb(0,40);
+    Person.Three.crd[0] = getRandNumb(0,40);
+    Person.Three.crd[1] = getRandNumb(0,40);
+    Person.Four.crd[0] = getRandNumb(0,40);
+    Person.Four.crd[1] = getRandNumb(0,40);
+    Person.Five.crd[0] = getRandNumb(0,40);
+    Person.Five.crd[1] = getRandNumb(0,40);
+    Person.User.crd[0] = getRandNumb(0,40);
+    Person.User.crd[1] = getRandNumb(0,40);
+}
+void movPers(person &Person){
+    // init coordinates crd[x,y]
+    Person.One.crd[0] = getRandNumb(0,40);
+    Person.One.crd[1] = getRandNumb(0,40);
+    Person.Two.crd[0] = getRandNumb(0,40);
+    Person.Two.crd[1] = getRandNumb(0,40);
+    Person.Three.crd[0] = getRandNumb(0,40);
+    Person.Three.crd[1] = getRandNumb(0,40);
+    Person.Four.crd[0] = getRandNumb(0,40);
+    Person.Four.crd[1] = getRandNumb(0,40);
+    Person.Five.crd[0] = getRandNumb(0,40);
+    Person.Five.crd[1] = getRandNumb(0,40);
+}
+void showField(person &Person){
+    for (int y = 40; y >= 0; y--){
+        for (int x = 0; x < 40; x++){
+            if (Person.One.crd[0] == x && Person.One.crd[1] == y){
+                cout << "E";
+            }else if (Person.Two.crd[0] == x && Person.Two.crd[1] == y){
+                cout << "E";
+            }else if (Person.Three.crd[0] == x && Person.Three.crd[1] == y){
+                cout << "E";
+            }else if (Person.Four.crd[0] == x && Person.Four.crd[1] == y){
+                cout << "E";
+            }else if (Person.Five.crd[0] == x && Person.Five.crd[1] == y){
+                cout << "E";
+            }else if (Person.User.crd[0] == x && Person.User.crd[1] == y){
+                cout << "P";
+            }else cout << ".";
+        }
+        cout << endl;
+    }
+}
+void analysisCourse(person &Person){
+    if (Person.User.crd[0] == Person.One.crd[0] && Person.One.crd[1] == Person.User.crd[1]){
 
+    }
 }
 int main(){
     srand(time(nullptr));
     getRandNumb(2,7);
     person Person;
-    initEnemy(Person);
-    cout << 'q';
+
+    cout << "Enter name player\n";
+    cin >> Person.User.name;
+    cout << "How many HP ?(50-150)\n";
+    cin >> Person.User.hp;
+    if (Person.User.hp < 50 || Person.User.hp > 150){
+        cout << "error hp\n";
+        return -1;
+    }
+    cout << "How many damage?(15-30)\n";
+    cin >> Person.User.damage;
+    if (Person.User.damage < 15 || Person.User.damage > 30){
+        cout << "error damage\n";
+        return -1;
+    }
+    cout << "How many armor?(0-50)\n";
+    cin >> Person.User.armor;
+    if (Person.User.armor < 0 || Person.User.armor > 50){
+        cout << "error damage\n";
+        return -1;
+    }
+    string option;
+    while (option != "exit"){
+        cout << "Enter option - game, save, load, exit\n";
+        cin >> option;
+        if (option == "game"){
+            initEnemy(Person);
+            showField(Person);
+            cout << "Enter action - left, right, top, bottom\n" << "or exit, save\n";
+            string action;
+            while (action != "exit"){
+                cout << "Enter action\n";
+                cin >> action;
+                if (action == "left"){
+                    Person.User.crd[0]--;
+                }else if (action == "right"){
+                    Person.User.crd[0]++;
+                }else if (action == "top"){
+                    Person.User.crd[1]++;
+                }else if (action == "bottom"){
+                    Person.User.crd[1]--;
+                }else if (action == "exit"){
+                    cout << "Exit game\n";
+                    break;
+                }else {
+                    cout << "no correct action\n";
+                    continue;
+                }
+                movPers(Person);
+                showField(Person);
+            }
+        }
+    }
+
     return 0;
 }
